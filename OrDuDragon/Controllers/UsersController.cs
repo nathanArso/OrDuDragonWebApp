c5ab63a260333b5ada645415ba63a1635eea98c1
nathanArso <nathan.arseneau.ca@gmail.com>
2016-05-02 14:49:45 -0400

    Login
    design login

---
+++ OrDuDragon/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OrDuDragon.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+    }
+}
---
+++ OrDuDragon/Models/Users.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrDuDragon.Models
+{
+    public class UserViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Nom d'usager")]
+        [Required]
+        [StringLength(50)]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "La chaîne {0} doit comporter au moins {2} caractères.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mot de passe")]
+        public string Password { get; set; }
+
+    }
+    public class User
+    {
+
+    }
+    public class Users
+    {
+
+    }
+
+
+}
