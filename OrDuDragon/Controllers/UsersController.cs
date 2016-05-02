0c96c78592b0930d504b3413eed4fc931f3d62b2
nathanArso <nathan.arseneau.ca@gmail.com>
2016-05-02 15:05:20 -0400

    Register
    Design register

--- OrDuDragon/Controllers/UsersController.cs
+++ OrDuDragon/Controllers/UsersController.cs
@@ -13,5 +13,9 @@ namespace OrDuDragon.Controllers
         {
             return View();
         }
+        public ActionResult Register()
+        {
+            return View();
+        }
     }
 }
--- OrDuDragon/Models/Users.cs
+++ OrDuDragon/Models/Users.cs
@@ -16,12 +16,26 @@ namespace OrDuDragon.Models
         [StringLength(50)]
         public string UserName { get; set; }
 
+        [Display(Name = "Prénom")]
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Nom")]
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
         [Required]
         [StringLength(50, ErrorMessage = "La chaîne {0} doit comporter au moins {2} caractères.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Mot de passe")]
         public string Password { get; set; }
 
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmer")]
+        [Compare("Password", ErrorMessage = "Le mot de passe et celui de confirmation ne correspondent pas.")]
+        public string ConfirmPassword { get; set; }
     }
     public class User
     {
